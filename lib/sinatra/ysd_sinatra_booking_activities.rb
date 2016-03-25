79c3f84df09a03fb23dc6d3cf9737ec9f5f1ebe3
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2016-03-25 14:55:23 +0100

    activities availability + menu

--- lib/sinatra/ysd_sinatra_booking_activities.rb
+++ lib/sinatra/ysd_sinatra_booking_activities.rb
@@ -13,13 +13,26 @@ module Sinatra
         app.post '/p/activity/add-to-shopping-cart' do 
 
           activity_id = params[:activity_id]
-          date = DateTime.strptime(params[:date],'%Y-%m-%d')
-          time = params[:turn]
+          date = time = description = nil
+
+          if params[:activity_date_id]
+            if activity_date = ::Yito::Model::Booking::ActivityDate.get(params[:activity_date_id])
+              date = activity_date.date_from
+              time = activity_date.time_from
+              description = activity_date.description
+            end
+          else
+            date = DateTime.strptime(params[:date],'%Y-%m-%d')
+            time = params[:turn]
+          end
           quantity_rate_1 = params[:quantity_rate_1].to_i
           quantity_rate_2 = params[:quantity_rate_2].to_i
           quantity_rate_3 = params[:quantity_rate_3].to_i
 
           if activity = ::Yito::Model::Booking::Activity.get(activity_id)
+
+            activity_name = activity.name
+            activity_name << " (#{description})" unless description.nil?
             
             # Load or build the shopping cart
             @shopping_cart = nil
@@ -37,7 +50,7 @@ module Sinatra
               @shopping_cart.add_item(date, 
                                       time, 
                                       activity.code, 
-                                      activity.name,
+                                      activity_name,
                                       1,
                                       quantity_rate_1,
                                       activity.rates(date)[1][1],
@@ -48,7 +61,7 @@ module Sinatra
               @shopping_cart.add_item(date, 
                                       time, 
                                       activity.code, 
-                                      activity.name,
+                                      activity_name,
                                       2,
                                       quantity_rate_2,
                                       activity.rates(date)[2][1],
@@ -59,7 +72,7 @@ module Sinatra
               @shopping_cart.add_item(date, 
                                       time, 
                                       activity.code, 
-                                      activity.name,
+                                      activity_name,
                                       3,
                                       quantity_rate_3,
                                       activity.rates(date)[3][1],
@@ -186,14 +199,24 @@ module Sinatra
           load_page(:reservation_activities)
 
         end
-        
+
         #
         # Show an activity
         #
         app.get '/p/activity/:id/?*' do
 
           @activity = ::Yito::Model::Booking::Activity.get(params[:id])
+          @occupation = []
           
+          case @activity.occurence
+            when :one_time
+              @occupation = @activity.occupation(@activity.date_from, @activity.time_from)
+            when :multiple_dates
+              @occupation = {total_occupation: 0, occupation_detail: {}}
+            when :cyclic
+              @occupation = {total_occupation: 0, occupation_detail: {}}
+          end
+
           if @activity and @activity.active
             load_page(:reservation_activity)
           else
@@ -201,6 +224,32 @@ module Sinatra
           end
 
         end
+      
+        #
+        # Select the date and/or turn
+        #
+        app.post '/p/activity/select-date/?*' do
+
+          if @activity = ::Yito::Model::Booking::Activity.get(params[:activity_id]) and @activity.active
+            @occupation = {total_occupation: 0, occupation_detail: {}}
+            if params[:activity_date_id]
+              if @activity_date = ::Yito::Model::Booking::ActivityDate.get(params[:activity_date_id])
+                @occupation = @activity.occupation(@activity_date.date_from, @activity_date.time_from)
+              end
+            elsif params[:date] or params[:time]
+              @date = params[:date]
+              @time = params[:turn]
+              if @date and !@date.nil? and @time and !@time.nil?
+                @occupation = @activity.occupation(@date, @time)
+              end
+            end
+            load_page(:reservation_activity)
+          else
+            status 404
+          end
+              
+        end
+
 
       end
 
